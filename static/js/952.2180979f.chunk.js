669c7081d7d10ced1916ba6da16c185112420263
PARAPOPOVICH <139543918+KOSFin@users.noreply.github.com>
2024-06-10 22:19:16 +0300

    preparing for the build

---
+++ static/js/952.2180979f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksynask_app=self.webpackChunksynask_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.2180979f.chunk.js.map
